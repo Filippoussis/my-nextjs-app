3ba0ddb079a2b99f9c55f901707d640ab6b4a4e9
Andrey Filippov <androsphilippos@gmail.com>
2022-11-04 22:06:35 +0500

    feat: create hoc withLayout

---
+++ hocs/withLayout.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react';
+import Layout from '../layouts/Layout/Layout';
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+  return function withLayoutComponent(props: T) {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
+};
